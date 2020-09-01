67d3201623816360aba6c89d0c98fd54bc0e51cf
Lachlan Glen <lachlanjglen@gmail.com>
2020-09-01 14:42:48 -0400

    added video file to assets

--- components/Sync.js
+++ components/Sync.js
@@ -11,12 +11,9 @@ let date2 = 0;
 
 const Sync = () => {
 
-  const videoURL = 'https://duette.s3.us-east-2.amazonaws.com/35b48b94-a89c-4014-9d46-e16b4cef43c9.mov';
-
   let screenWidth = Math.floor(Dimensions.get('window').width);
   let screenHeight = Math.floor(Dimensions.get('window').height);
 
-  const [vidUri, setVidUri] = useState('');
   const [vidARef, setVidARef] = useState(null);
   const [vidBRef, setVidBRef] = useState(null);
   const [vid1Ready, setVid1Ready] = useState(false);
@@ -25,29 +22,6 @@ const Sync = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [previewComplete, setPreviewComplete] = useState(false);
   const [customOffset, setCustomOffset] = useState(0);
-
-  useEffect(() => {
-    const saveVideoLocally = async () => {
-      const { uri } = await FileSystem.downloadAsync(
-        videoURL,
-        FileSystem.documentDirectory + 'vid.mov'
-      );
-      setVidUri(uri);
-    }
-    saveVideoLocally();
-  }, []);
-
-  // const handleProblem = () => {
-  //   Alert.alert(
-  //     'What would you like to do?',
-  //     "'Soft Refresh' is recommended if a video has frozen. 'Hard Refresh' is recommended if a video has failed to load.",
-  //     [
-  //       { text: 'Soft Refresh', onPress: () => handleRestart() },
-  //       { text: 'Hard Refresh', onPress: () => handleHardRefresh() },
-  //     ],
-  //     { cancelable: false }
-  //   );
-  // };
 
   const handlePlaybackStatusUpdate = (updateObj, whichVid) => {
     if (whichVid === 'vid1') {
@@ -149,193 +123,183 @@ const Sync = () => {
   console.log('delay: ', date2 - date1);
 
   return (
-    !vidUri ? (
-      <View>
-        <Text>Loading...</Text>
-      </View>
-    ) : (
-        <ScrollView style={{
-          ...styles.container,
-          paddingTop: previewComplete ? (screenHeight - (screenWidth / 8 * 9)) / 2 : 0,
-        }}>
-          <View style={{
-            flexDirection: 'row',
-            marginTop: !previewComplete && Platform.OS === 'ios' ? 20 : 0,
-          }}>
-            <Video
-              ref={ref => setVidARef(ref)}
-              source={{
-                uri: vidUri,
-              }}
-              rate={1.0}
-              volume={1.0}
-              isMuted={false}
-              resizeMode="cover"
-              shouldPlay={false}
-              // positionMillis={0}
-              isLooping={false}
+    <ScrollView style={{
+      ...styles.container,
+      paddingTop: previewComplete ? (screenHeight - (screenWidth / 8 * 9)) / 2 : 0,
+    }}>
+      <View style={{
+        flexDirection: 'row',
+        marginTop: !previewComplete && Platform.OS === 'ios' ? 20 : 0,
+      }}>
+        <Video
+          ref={ref => setVidARef(ref)}
+          source={require('../assets/L-O-V-E.mov')}
+          rate={1.0}
+          volume={1.0}
+          isMuted={false}
+          resizeMode="cover"
+          shouldPlay={false}
+          // positionMillis={0}
+          isLooping={false}
+          style={{
+            width: screenWidth / 2,
+            height: screenWidth / 16 * 9,
+          }}
+          onPlaybackStatusUpdate={update => handlePlaybackStatusUpdate(update, 'vid1')}
+        />
+        <Video
+          ref={ref => setVidBRef(ref)}
+          source={require('../assets/L-O-V-E.mov')}
+          rate={1.0}
+          volume={1.0}
+          isMuted={false}
+          resizeMode="cover"
+          shouldPlay={false}
+          isLooping={false}
+          style={{
+            width: screenWidth / 2,
+            height: screenWidth / 16 * 9,
+          }}
+          onPlaybackStatusUpdate={update => handlePlaybackStatusUpdate(update, 'vid2')}
+        />
+        {
+          // if preview hasn't played yet
+          !previewComplete && !isPlaying ? (
+            <TouchableOpacity
+              onPress={handleShowPreview}
               style={{
-                width: screenWidth / 2,
-                height: screenWidth / 16 * 9,
-              }}
-              onPlaybackStatusUpdate={update => handlePlaybackStatusUpdate(update, 'vid1')}
-            />
-            <Video
-              ref={ref => setVidBRef(ref)}
-              source={{
-                uri: vidUri,
-              }}
-              rate={1.0}
-              volume={1.0}
-              isMuted={false}
-              resizeMode="cover"
-              shouldPlay={false}
-              isLooping={false}
-              style={{
-                width: screenWidth / 2,
-                height: screenWidth / 16 * 9,
-              }}
-              onPlaybackStatusUpdate={update => handlePlaybackStatusUpdate(update, 'vid2')}
-            />
-            {
-              // if preview hasn't played yet
-              !previewComplete && !isPlaying ? (
-                <TouchableOpacity
-                  onPress={handleShowPreview}
-                  style={{
-                    ...styles.overlay,
-                    width: screenWidth,
-                    height: screenWidth / 16 * 9
-                  }}>
-                  <View style={{ flexDirection: 'row' }}>
-                    <Text style={{
-                      fontSize: screenWidth / 20,
-                      fontWeight: 'bold',
-                      marginRight: 10,
-                    }}>
-                      {bothVidsReady ? 'Touch to preview!' : 'Loading...'}
-                    </Text>
-                    {
-                      !bothVidsReady &&
-                      <ActivityIndicator size="small" color="#0047B9" />
-                    }
-                  </View>
-                </TouchableOpacity>
-              ) : (
-                  // if preview has played (previewComplete)
-                  previewComplete &&
-                  <TouchableOpacity
-                    style={{
-                      ...styles.overlay,
-                      opacity: 0.8,
-                      paddingTop: 40,
-                      flexDirection: 'column',
-                      justifyContent: 'space-evenly',
-                      width: screenWidth,
-                      height: screenWidth / 16 * 9,
-                    }}>
-                    <TouchableOpacity
-                      style={{
-                        ...styles.button,
-                        width: 200,
-                      }}
-                      onPress={handleShowPreview}>
-                      <Text
-                        style={styles.buttonText}>
-                        View again
-                  </Text>
-                    </TouchableOpacity>
-                  </TouchableOpacity>
-                )
-            }
-          </View>
-          {
-            !previewComplete &&
-            <View>
-              <Text
-                style={styles.instruction}>Not perfectly in sync? Use the arrows below to adjust to your taste!
-          </Text>
-              <View
-                style={styles.syncIconsContainer}>
-                <Icon
-                  onPress={isPlaying ? handleSyncBack : () => { }}
-                  underlayColor="black"
-                  name="fast-rewind"
-                  type="material"
-                  color="white"
-                  size={50} />
-                <Text
-                  style={{
-                    fontSize: 25,
-                    color: 'white',
-                    alignSelf: 'center',
-                  }}>|
-            </Text>
-                <Icon
-                  onPress={isPlaying ? handleSyncForward : () => { }}
-                  underlayColor="black"
-                  name="fast-forward"
-                  type="material"
-                  color="white"
-                  size={50} />
-              </View>
-              <Text
-                style={styles.hintTitle}>Hint:
-          </Text>
-              <View
-                style={styles.hintContainer}>
-                <Text
-                  style={{ color: 'white', fontSize: 14, }}>If the right video is <Text style={{ color: 'yellow' }}>behind</Text> the left video, press
-            </Text>
-                <Icon
-                  name="fast-forward"
-                  type="material"
-                  color="yellow" />
-              </View>
-              <View
-                style={{
-                  ...styles.hintContainer,
-                  marginBottom: 10,
-
-                }}>
-                <Text
-                  style={{ color: 'white', fontSize: 14, }}>If the right video is <Text style={{ color: 'yellow' }}>ahead of</Text> the left video, press
-            </Text>
-                <Icon
-                  name="fast-rewind"
-                  type="material"
-                  color="yellow" />
-              </View>
-              <View style={{
-                flexDirection: 'row',
-                justifyContent: 'space-evenly',
+                ...styles.overlay,
+                width: screenWidth,
+                height: screenWidth / 16 * 9
               }}>
+              <View style={{ flexDirection: 'row' }}>
+                <Text style={{
+                  fontSize: screenWidth / 20,
+                  fontWeight: 'bold',
+                  marginRight: 10,
+                }}>
+                  {bothVidsReady ? 'Touch to preview!' : 'Loading...'}
+                </Text>
+                {
+                  !bothVidsReady &&
+                  <ActivityIndicator size="small" color="#0047B9" />
+                }
+              </View>
+            </TouchableOpacity>
+          ) : (
+              // if preview has played (previewComplete)
+              previewComplete &&
+              <TouchableOpacity
+                style={{
+                  ...styles.overlay,
+                  opacity: 0.8,
+                  paddingTop: 40,
+                  flexDirection: 'column',
+                  justifyContent: 'space-evenly',
+                  width: screenWidth,
+                  height: screenWidth / 16 * 9,
+                }}>
                 <TouchableOpacity
                   style={{
                     ...styles.button,
-                    width: 100,
-                    marginBottom: 10,
+                    width: 200,
                   }}
-                  onPress={handleShowPreview} >
+                  onPress={handleShowPreview}>
                   <Text
-                    style={{
-                      ...styles.buttonText,
-                      fontSize: Platform.OS === 'ios' ? 20 : 17,
-                      fontWeight: Platform.OS === 'ios' ? 'normal' : 'bold',
-                    }}>{!previewComplete && !isPlaying && bothVidsReady ? 'Preview' : 'Restart'}
+                    style={styles.buttonText}>
+                    View again
                   </Text>
                 </TouchableOpacity>
-              </View>
-              {/* <TouchableOpacity
+              </TouchableOpacity>
+            )
+        }
+      </View>
+      {
+        !previewComplete &&
+        <View>
+          <Text
+            style={styles.instruction}>Not perfectly in sync? Use the arrows below to adjust to your taste!
+          </Text>
+          <View
+            style={styles.syncIconsContainer}>
+            <Icon
+              onPress={isPlaying ? handleSyncBack : () => { }}
+              underlayColor="black"
+              name="fast-rewind"
+              type="material"
+              color="white"
+              size={50} />
+            <Text
+              style={{
+                fontSize: 25,
+                color: 'white',
+                alignSelf: 'center',
+              }}>|
+            </Text>
+            <Icon
+              onPress={isPlaying ? handleSyncForward : () => { }}
+              underlayColor="black"
+              name="fast-forward"
+              type="material"
+              color="white"
+              size={50} />
+          </View>
+          <Text
+            style={styles.hintTitle}>Hint:
+          </Text>
+          <View
+            style={styles.hintContainer}>
+            <Text
+              style={{ color: 'white', fontSize: 14, }}>If the right video is <Text style={{ color: 'yellow' }}>behind</Text> the left video, press
+            </Text>
+            <Icon
+              name="fast-forward"
+              type="material"
+              color="yellow" />
+          </View>
+          <View
+            style={{
+              ...styles.hintContainer,
+              marginBottom: 10,
+
+            }}>
+            <Text
+              style={{ color: 'white', fontSize: 14, }}>If the right video is <Text style={{ color: 'yellow' }}>ahead of</Text> the left video, press
+            </Text>
+            <Icon
+              name="fast-rewind"
+              type="material"
+              color="yellow" />
+          </View>
+          <View style={{
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+          }}>
+            <TouchableOpacity
+              style={{
+                ...styles.button,
+                width: 100,
+                marginBottom: 10,
+              }}
+              onPress={handleShowPreview} >
+              <Text
+                style={{
+                  ...styles.buttonText,
+                  fontSize: Platform.OS === 'ios' ? 20 : 17,
+                  fontWeight: Platform.OS === 'ios' ? 'normal' : 'bold',
+                }}>{!previewComplete && !isPlaying && bothVidsReady ? 'Preview' : 'Restart'}
+              </Text>
+            </TouchableOpacity>
+          </View>
+          {/* <TouchableOpacity
                 onPress={isPlaying ? handleProblem : handleHardRefresh}
                 style={styles.problemContainer}
               >
                 <Text style={{ fontSize: 16, color: 'red' }}>Touch here to refresh.</Text>
               </TouchableOpacity> */}
-            </View>
-          }
-        </ScrollView>
-      )
+        </View>
+      }
+    </ScrollView>
   )
 };
 
